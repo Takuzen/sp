bc71a1a4fdbfc029b44f7287f798fb3be93a9502
Takuzen Toh <takuzen0430@gmail.com>
2023-09-29 17:50:49 +0900

    feat: basement

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Enable Model Viewer */}
+        <script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.1.1/model-viewer.min.js" async></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
