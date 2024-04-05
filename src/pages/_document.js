81ef0b96d60fce9cc32a63a80cc53ea3bd15c95f
Takuzen Toh <takuzen0430@gmail.com>
2024-04-05 15:44:02 -0700

    feat: add ogp again with head tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta property="og:title" content="NoteSP" />
+        <meta property="og:description" content="Spatial Notes, Made Easy." />
+        <meta property="og:image" content="/notesp-logo.png" />
         {/* Enable Model Viewer */}
         <script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.1.1/model-viewer.min.js" async></script>
       </Head>
